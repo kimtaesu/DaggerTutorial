c9f572cdcbc00b47bdd037d6eb5c6dbe27e4ff16
kimtaesu <kimtaesoo188@gmail.com>
2017-08-22 11:53:49 +0900

    Revert CoffeMaker2

--- app/src/main/java/com/hucet/dagger/di/ApplicationComponent.kt
+++ app/src/main/java/com/hucet/dagger/di/ApplicationComponent.kt
@@ -1,7 +1,6 @@
 package com.hucet.dagger.di
 
-import com.hucet.dagger.inject.field.CoffeMaker2
-import com.hucet.dagger.provide.DripCoffeeModule
+import com.hucet.dagger.inject.field.CoffeMaker
 import com.hucet.dagger.provide.DripCoffeeModuleJava
 import dagger.Component
 import javax.inject.Singleton
@@ -13,8 +12,8 @@ import javax.inject.Singleton
 @Component(modules = arrayOf(DripCoffeeModuleJava::class))
 //@Component(modules = arrayOf(DripCoffeeModule::class))
 interface CoffeeShop {
-    //    fun maker(): CoffeMaker
-    fun maker(): CoffeMaker2
+    fun maker(): CoffeMaker
+//    fun maker(): CoffeMaker2
 }
 
 class Foo {
